c1f73b91c4869d0def73a869153f6c3712e8c114
lucas_linux <lucasthiele0@outlook.com>
2021-06-23 23:12:24 -0300

    Pagina de Admin, Botao de like

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 //Dependências
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-//Componentes
+//Pages
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 import { AuthContextProvider } from './contexts/AuthContext'
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
           <Route path="/" exact component={Home}></Route>
           <Route path="/rooms/new" exact component={NewRoom}></Route>
           <Route path="/rooms/:id" component={Room}></Route>
+
+          <Route path="/admin/rooms/:id" component={AdminRoom}></Route>
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
